6840dff5b7c096fac5f1369ed728d05cabaffc22
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-10-23 19:46:57 +0200

    index.js edit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('funguju!');
+const Task = (props) => {
+    fetch ('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks').then((response).json()).then(data => {
+        data.map((item) =>{
+
+        })
+    }).join('');
+
+
+};
+
+
