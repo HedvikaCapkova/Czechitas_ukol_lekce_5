0ca99f403b09ffb73ba6314dc0e884ffab69fe85
HedvikaCapkova <hedvika.capkova@gmail.com>
2022-10-28 18:39:46 +0200

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const Task = (props) => {
-    fetch ('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks').then((response).json()).then(data => {
-        data.map((item) =>{
-
-        })
-    }).join('');
-
-
+const renderTasks = () => {
+    fetch ('https://apps.kodim.cz/daweb/trening-api/apis/tasks-api/tasks').then((response)=>{
+        return response.json();
+    }) 
+    .then(data => {
+        const taskList = document.querySelector('.task');
+        taskList.innerHtml = data.map((item) => {
+            return Task(item);
+        }).join('');
+    });
 };
 
+renderTasks();
+
+const Task = (props) => {
+    const { name, due, done } = prosp;
+
+    return`
+        <div class="task__body">
+          <div class="task__name">${name}</div>
+          <div class="task__due">${due}</div>
+          </div>
+        <div class="task__done">${done}</div>
+        `;     
+};
 
